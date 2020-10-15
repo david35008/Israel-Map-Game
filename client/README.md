a131d29ca46ab6246a85779280333c7533d0a4dc
david35008 <67857450+david35008@users.noreply.github.com>
2020-10-15 14:32:27 +0300

    initial

---
+++ client/README.md
@@ -0,0 +1,1 @@
+# Google-map-game
---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    Hello Wrold
+    </div>
+  );
+}
+
+export default App;
